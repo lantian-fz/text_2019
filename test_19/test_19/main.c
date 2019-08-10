5e6324b4bd3a0ce9db4cd85f9ddeb17288f8542b
lantian-fz <2459396922@qq.com>
2019-08-10 22:15:45 +0800

    练习代码

---
+++ test_19/test_19/main.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int arr[50][50] = { 0 };
+	int i = 0;
+	int j = 0;
+	int n = 0;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		arr[i][0] = 1;
+	}
+
+	for (i = 1; i < n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+		}
+	}
+	printf("%d½×Ñî»ÔÈý½Ç£º\n", n);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+			printf("%-4d ", arr[i][j]);
+		printf("\n");
+	}
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
