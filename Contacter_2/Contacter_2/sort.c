3bcdb704f1041a83cfc195f6c3a4c8f674e6ae38
lantian-fz <2459396922@qq.com>
2019-08-15 09:09:23 +0800

    练习代码

--- Contacter_2/Contacter_2/sort.c
+++ Contacter_2/Contacter_2/sort.c
@@ -2,22 +2,9 @@
 
 #include "contact.h"
 
-int my_strcmp(const char* str1, const char* str2)
-{
-	assert(str1 != NULL);
-	assert(str2 != NULL);
-
-	while (*str1 && *str1 == *str2)
-		str1++, str2++;
-
-	if (!*str1&&!*str2)
-		return 0;
-	return *str1 - *str2;
-}
-
 static int Name(const void* e1, const void* e2)
 {
-	return my_strcmp((char*)e1, (char*)e2);
+	return strcmp(((Person *)e1)->name, ((Person *)e2)->name);
 }
 
 void SortName(Contact *con)
@@ -29,7 +16,7 @@ void SortName(Contact *con)
 
 static int Age(const void* e1, const void* e2)
 {
-	return ((Contact*)e1)->data->age - ((Contact*)e2)->data->age;
+	return ((Person*)e1)->age - ((Person*)e2)->age;
 }
 
 void SortAge(Contact *con)
@@ -41,7 +28,7 @@ void SortAge(Contact *con)
 
 static int Phone(const void* e1, const void* e2)
 {
-	return strcmp((char*)e1, (char*)e2);
+	return strcmp(((Person*)e1)->phone, ((Person*)e2)->phone);
 }
 
 void SortPhone(Contact *con)
