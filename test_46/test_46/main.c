0823dccf848149665f622edbc5444efe5be10f04
lantian-fz <2459396922@qq.com>
2019-08-30 22:45:52 +0800

    练习代码

---
+++ test_46/test_46/main.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a, b, c, d, e;
+	for (a = 1; a <= 5; a++)
+	{
+		for (b = 1; b <= 5; b++)
+		{
+			for (c = 1; c <= 5; c++)
+			{
+				for (d = 1; d <= 5; d++)
+				{
+					for (e = 1; e <= 5; e++)
+					{
+						if (a*b*c*d*e == 120)
+						{
+							if (((b == 2) + (a == 3) == 1) &&
+								((b == 2) + (e == 4) == 1) &&
+								((c == 1) + (d == 2) == 1) &&
+								((c == 5) + (d == 3) == 1) &&
+								((e == 4) + (a == 1) == 1))
+							{
+								printf("A = %d,B = %d,C = %d,D = %d,E = %d\n", a, b, c, d, e);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
