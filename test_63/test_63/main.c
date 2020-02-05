ba27485564b94a3cf389bd6188139705edafd561
lantian-fz <2459396922@qq.com>
2020-02-05 23:26:36 +0800

    练习代码

---
+++ test_63/test_63/main.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+//说明 :初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+//你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+//示例 :输入 :nums1 = [1, 2, 3, 0, 0, 0], m = 3
+//	        nums2 = [2, 5, 6], n = 3
+//      输出 : [1, 2, 2, 3, 5, 6]
+#include <stdio.h>
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
+{
+	//由于nums1有足够的空间，那可以比较两个数组最后两个元素后，最大的从下标m+n-1处放置元素
+	//两个数组已经有序，元素之间需要比较m+n-1次
+	int x = m-1;
+	int y = n-1;
+	int count = x + y - 1;
+	if (m == 0)
+	{
+		for (int i = 0; i< n; i++)
+		{
+			nums1[i] = nums2[i];
+		}
+		return;
+	}
+	for (int i = count; i > 0; i--)
+	{
+		if (nums1[x] < nums2[y])
+		{
+			nums1[count--] = nums2[y];
+			y--;
+		}
+		else
+		{
+			nums1[count--] = nums1[x];
+			x--;
+		}
+	}
+}
+
+void main()
+{
+	int nums1[6] = { 1,2,3};
+	int nums2[3] = { 2,5,6 };
+	int m = 3;
+	int n = 3;
+	merge(nums1, sizeof(nums1) / sizeof(nums1[0]), m, nums2, sizeof(nums2) / sizeof(nums2[0]), n);
+	for (int i = 0; i < (m + n); i++)
+	{
+		printf("%d ", nums1[i]);
+	}
+	printf("\n");
+}
