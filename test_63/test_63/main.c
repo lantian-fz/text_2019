67c017a9d934defbc5e7122b1387de307fe5177b
lantian-fz <2459396922@qq.com>
2020-02-07 12:00:43 +0800

    练习代码

--- test_63/test_63/main.c
+++ test_63/test_63/main.c
@@ -11,42 +11,58 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
 {
 	//由于nums1有足够的空间，那可以比较两个数组最后两个元素后，最大的从下标m+n-1处放置元素
 	//两个数组已经有序，元素之间需要比较m+n-1次
-	int x = m-1;
-	int y = n-1;
-	int count = x + y - 1;
-	if (m == 0)
+	int x = m - 1; 
+	int y = n - 1; 
+	int count = m + n - 1;
+	while (y >= 0)
 	{
-		for (int i = 0; i< n; i++)
+		if (x < 0)
 		{
-			nums1[i] = nums2[i];
-		}
-		return;
-	}
-	for (int i = count; i > 0; i--)
-	{
-		if (nums1[x] < nums2[y])
-		{
-			nums1[count--] = nums2[y];
-			y--;
+			nums1[count--] = nums2[count--];
 		}
 		else
 		{
-			nums1[count--] = nums1[x];
-			x--;
+			if (nums1[x] >= nums2[y])
+				nums1[count--] = nums1[x--];
+			else
+				nums1[count--] = nums2[y--];
 		}
 	}
+}
+int func(int a)
+{
+	int b;
+	switch (a)
+	{
+	case 1: b = 30;
+	case 2: b = 20;
+	case 3: b = 16;
+	default: b = 0;
+	} 
+	return b;
+}
+
+void test()
+{
+	printf("%d\n", func(1));
 }
 
 void main()
 {
-	int nums1[6] = { 1,2,3};
-	int nums2[3] = { 2,5,6 };
-	int m = 3;
-	int n = 3;
-	merge(nums1, sizeof(nums1) / sizeof(nums1[0]), m, nums2, sizeof(nums2) / sizeof(nums2[0]), n);
-	for (int i = 0; i < (m + n); i++)
-	{
-		printf("%d ", nums1[i]);
-	}
+	//int nums1[6] = { 1,2,3};
+	//int nums2[3] = { 2,5,6 };
+	//int m = 3;
+	//int n = 3;
+	//merge(nums1, sizeof(nums1) / sizeof(nums1[0]), m, nums2, sizeof(nums2) / sizeof(nums2[0]), n);
+	//for (int i = 0; i < (m + n); i++)
+	//{
+	//	printf("%d ", nums1[i]);
+	//}
+
+	//int *p, a = 10;
+	//p = &a;
+	//printf("%d\n", &*p);
+	test();
+
 	printf("\n");
 }
